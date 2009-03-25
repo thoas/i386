5e09bfba5a5b6a473cf7e2811bf98bdb6cbe0b6c
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-03-25 22:15:26 +0000

    update settings.py

--- trunk/settings.py
+++ trunk/settings.py
@@ -29,7 +29,7 @@ TIME_ZONE = 'Europe/Paris'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'fr-FR'
+LANGUAGE_CODE = 'fr'
 
 SITE_ID = 1
 
