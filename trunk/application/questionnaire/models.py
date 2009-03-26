3618df7fb7f00278486e09e828a0d3e4e0704826
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-03-26 09:34:19 +0000

    fix questionnaire

--- trunk/application/questionnaire/models.py
+++ trunk/application/questionnaire/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.forms import ModelForm
+from django.forms import forms
+from django.forms import ModelForm, Textarea
 from django.utils.translation import ugettext_lazy as _
 
 PROFESSION_CHOICES = (
@@ -14,12 +15,10 @@ PROFESSION_CHOICES = (
 )
 
 class Enquete(models.Model):
-    first_name = models.CharField(_('Nom'), max_length=255, null=False, blank=False)
-    last_name = models.CharField(_('Prenom'), max_length=255, null=False, blank=False)
-    nickname = models.CharField(_('Pseudonyme'), max_length=255, null=True, blank=True)
+    coordonnees = models.CharField(_('Nom / Prenom / Pseudonyme'), max_length=255, null=False, blank=False)
     age = models.CharField(_('Age'), max_length=255, null=True, blank=True)
     email = models.EmailField(_('Mail'), max_length=255, null=False, blank=False)
-    profession = models.CharField(_('Profession'), max_length=20, choices=PROFESSION_CHOICES, null=True, blank=True)
+    profession = models.CharField(_('Profession'), max_length=20, null=True, blank=True)
     place_to_work = models.CharField(_('Societe'), max_length=255, null=True, blank=True)
     
     question1 = models.TextField(_('Question 1'), null=True, blank=True)
@@ -35,3 +34,7 @@ class Enquete(models.Model):
 class EnqueteForm(ModelForm):
     class Meta:
         model = Enquete
+    def __init__(self, *args, **kwargs):
+        super(EnqueteForm, self).__init__(*args, **kwargs)
+        for i in range(7):
+            self.fields['question%d' % (i + 1)].widget.attrs = {'rows': 5, 'cols': 53}
