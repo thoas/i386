775f7ee49d9bbe184a4ce6c36d9c49ba7c0de07a
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-03-29 15:58:24 +0000

    add pinax applications

--- application/survey/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('gobelins_project.application.survey.views',
-    (r'^$', 'index'),
-    (r'^thanks/$', 'thanks'),
-)
--- manage.py
+++ manage.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
+
+import sys
+from os.path import abspath, dirname, join
+from site import addsitedir
+
+PROJECT_ROOT = abspath(dirname(__file__))
+LIB_EXTERNAL_ROOT = join(PROJECT_ROOT, "libs", "externals")
+APPLICATION_ROOT = join(PROJECT_ROOT, 'application')
+
+
+path = addsitedir(LIB_EXTERNAL_ROOT, set())
+if path:
+    sys.path = list(path) + sys.path
+sys.path.insert(0, APPLICATION_ROOT)
+sys.path.insert(0, LIB_EXTERNAL_ROOT)
+sys.path.insert(0, join(APPLICATION_ROOT, 'internals'))
+sys.path.insert(0, join(APPLICATION_ROOT, 'externals'))
+
 from django.core.management import execute_manager
+
 try:
     import settings # Assumed to be in the same directory.
 except ImportError:
