3eeab396f339fdc6b2155ae5683b852980045a97
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-04-12 18:21:02 +0000

    add production and local settings

---
+++ trunk/manage_path.py
@@ -0,0 +1,16 @@
+import sys
+from os.path import abspath, dirname, join
+from site import addsitedir
+
+PROJECT_ROOT = abspath(dirname(__file__))
+LIB_EXTERNAL_ROOT = join(PROJECT_ROOT, "libs", "externals")
+APPLICATION_ROOT = join(PROJECT_ROOT, 'application')
+
+
+path = addsitedir(LIB_EXTERNAL_ROOT, set())
+if path:
+    sys.path = list(path) + sys.path
+sys.path.insert(0, APPLICATION_ROOT)
+sys.path.insert(0, LIB_EXTERNAL_ROOT)
+sys.path.insert(0, join(APPLICATION_ROOT, 'internals'))
+sys.path.insert(0, join(APPLICATION_ROOT, 'externals'))
---
+++ trunk/production_settings.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Django settings for basic pinax project.
+
+import os
+
+DEBUG = False
+
+DATABASE_ENGINE = 'mysql'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = 'gobelins_project'       # Or path to database file if using sqlite3.
+DATABASE_USER = 'gobelins_project'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'gobelinsrocks'         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from local_settings import *
+except ImportError:
+    pass
