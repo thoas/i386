d54f25435adbff08da206ffef8882757ca950b01
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-04-17 18:13:33 +0000

    fix test batch

--- image/tests.py
+++ image/tests.py
@@ -11,41 +11,45 @@ __DIR__ = os.path.dirname(os.path.abspath(__file__))
 BASE_ROOT = os.path.join(__DIR__, 'base')
 DEFAULT_TEST = 'thumbnail'
 
-def thumbnail(files):
-    for file in files:
-        im = Image.open(os.path.join(BASE_ROOT, file))
-        new_location = os.path.join(__DIR__, file)
-        if os.path.exists(new_location):
-            try:
-                os.remove(new_location)
-            except WindowsError:
-                print '%s est utilisé par un autre programme' % file
-        im.save(new_location, quality=95)
-
-def resize(files):
-    pass
+class Test():
+    @classmethod
+    def thumbnail(self, files):
+        for file in files:
+            im = Image.open(os.path.join(BASE_ROOT, file))
+            new_location = os.path.join(__DIR__, file)
+            if os.path.exists(new_location):
+                try:
+                    os.remove(new_location)
+                except WindowsError:
+                    print '%s est utilisé par un autre programme' % file
+            im.save(new_location, quality=95)
+    @classmethod
+    def resize(self, files):
+        pass
+        
+    @classmethod
+    def crop(self, files):
+        for file in files:
+            im = Image.open(os.path.join(BASE_ROOT, file))
+            # tuple : (100, 100, 800, 800) = 100px à gauche, 100px à partir du haut, jusqu'à 800px en bas et à droite.
+            crop = im.crop((100, 100, 800, 800))
+            crop.save(os.path.join(__DIR__, file), quality=95)
     
-def crop(files):
-    for file in files:
-        im = Image.open(os.path.join(BASE_ROOT, file))
-        # tuple : (100, 100, 800, 800) = 100px à gauche, 100px à partir du haut, jusqu'à 800px en bas et à droite.
-        crop = im.crop((100, 100, 800, 800))
-        crop.save(os.path.join(__DIR__, file), quality=95)
-
-def roll(image, delta):
-    "Roll an image sideways"
-
-    xsize, ysize = image.size
-
-    delta = delta % xsize
-    if delta == 0: return image
-
-    part1 = image.crop((0, 0, delta, ysize))
-    part2 = image.crop((delta, 0, xsize, ysize))
-    image.paste(part2, (0, 0, xsize-delta, ysize))
-    image.paste(part1, (xsize-delta, 0, xsize, ysize))
-
-    return image
+    @classmethod
+    def roll(self, image, delta):
+        "Roll an image sideways"
+    
+        xsize, ysize = image.size
+    
+        delta = delta % xsize
+        if delta == 0: return image
+    
+        part1 = image.crop((0, 0, delta, ysize))
+        part2 = image.crop((delta, 0, xsize, ysize))
+        image.paste(part2, (0, 0, xsize-delta, ysize))
+        image.paste(part1, (xsize-delta, 0, xsize, ysize))
+    
+        return image
         
 if __name__ == '__main__':
     usage = "usage: %prog [options]"
@@ -62,10 +66,8 @@ if __name__ == '__main__':
                                 + "(?P<extension>gif|jpg|png|tif)$", file) # ça rocks!
             if m:
                 files_list.append(file)
-    
-    if options.type == 'thumbnail':
-        thumbnail(files_list)
-    elif options.type == 'resize':
-        resize(files_list)
-    elif options.type == 'crop':
-        crop(files_list)
+             
+    try:   
+        getattr(Test, options.type)(files_list)
+    except AttributeError:
+        print "%s doesn\'t exist" % options.type
