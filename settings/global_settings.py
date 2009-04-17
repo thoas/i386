8c0a5e7a4ce4091511a3a6fa5c29c254cc7ff94d
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-04-17 08:01:35 +0000

    add lib dir to sys.path

--- settings/global_settings.py
+++ settings/global_settings.py
@@ -3,7 +3,8 @@ from os.path import abspath, dirname, join
 from site import addsitedir
 
 PROJECT_ROOT = abspath(dirname(__file__)) + '/..'
-LIB_EXTERNAL_ROOT = join(PROJECT_ROOT, "libs", "externals")
+LIB_ROOT = join(PROJECT_ROOT, "libs")
+LIB_EXTERNAL_ROOT = join(LIB_ROOT, "externals")
 APPLICATION_ROOT = join(PROJECT_ROOT, 'application')
 
 
@@ -11,6 +12,7 @@ path = addsitedir(LIB_EXTERNAL_ROOT, set())
 if path:
     sys.path = list(path) + sys.path
 sys.path.insert(0, APPLICATION_ROOT)
+sys.path.insert(0, LIB_ROOT)
 sys.path.insert(0, LIB_EXTERNAL_ROOT)
 sys.path.insert(0, join(APPLICATION_ROOT, 'internals'))
 sys.path.insert(0, join(APPLICATION_ROOT, 'externals'))
