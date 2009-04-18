9e28df23b7de83c7a8fe7a6aaa8dafd44b82f1a4
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-04-18 10:12:53 +0000

    rename case to square

--- application/internals/square/models.py
+++ application/internals/square/models.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 from chapter.models import Chapter
 from django.utils.translation import ugettext_lazy as _
 
-class Case(models.Model):
+class Square(models.Model):
     pos_x = models.IntegerField(_('pos_x'))
     pos_y = models.IntegerField(_('pos_y'))
     background_image_path = models.ImageField(upload_to=settings.UPLOAD_ROOT)
     date_booked = models.DateField(_('date_booked'), auto_now_add=True)
     date_finished = models.DateField(_('date_finished'))
     
-class ParticipateCase(models.Model):
+class ParticipateSquare(models.Model):
     user = models.ForeignKey(User, verbose_name=_('user'))
     case = models.ForeignKey(Case, verbose_name=_('case'))
     
