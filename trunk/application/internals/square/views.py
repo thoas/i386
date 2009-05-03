ac0964e037f82177d81007ed8099e3707d3a5012
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-05-03 18:22:26 +0000

    add template example

--- trunk/application/internals/square/views.py
+++ trunk/application/internals/square/views.py
@@ -18,7 +18,7 @@ from django.conf import settings
 def book(request, pos_x, pos_y):
     INK = "red", "blue", "green", "yellow", 'black'
     image = Image.new("RGB", (1200, 1200), random.choice(INK))
-    response = HttpResponse(mimetype="image/png")
+    response = HttpResponse(mimetype="image/tif")
     square = get_object_or_404(Square, pos_x=pos_x, pos_y=pos_y)
     neighbors_key = square.neighbors()
     neighbors = Square.objects.neighbors(square)
@@ -26,37 +26,17 @@ def book(request, pos_x, pos_y):
     for neighbor in neighbors:
         index = neighbors_key[neighbor.coord]
         im = Image.open(neighbor.background_image_path.path)
-        pos = POSITIONS[index]
-        size = SIZE[index]
-        crop_pos = (pos[0], pos[1], size[0] + pos[0], size[1] + pos[1])
         
-        print '%s -> %s (%s)' % (pos + size, CROP_POS[index], LITERAL[index])
+        print '%s -> %s (%s)' % (CROP_POS[index], PASTE_POS[index], LITERAL[index])
         crop = im.crop(CROP_POS[index])
         image.paste(crop, PASTE_POS[index])
-    
-    buffer = StringIO.StringIO()
-    image.save(buffer, format='PNG', quality=90)
-    jpeg = buffer.getvalue()
+
     now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    response.write(jpeg)
-    return response
-
-def test(request, pos_x, pos_y):
-    INK = "red", "blue", "green", "yellow"
-    image = Image.new("RGB", (800, 600), random.choice(INK))
-
-    # to save as a file
-    #import os.path
-    #from django.conf import settings
-    #IMAGE_PATH = os.path.join(settings.FILE_UPLOAD_TEMP_DIR, 'test.tif')
-    #image.save(IMAGE_PATH, "TIFF")
-
+    #response['Content-Disposition'] = 'attachment; filename=%s_x%s_y%s_%s_template.tif'\
+    #    % (request.user.username, pos_x, pos_y, now)
     buffer = StringIO.StringIO()
     image.save(buffer, format='TIFF', quality=90)
     jpeg = buffer.getvalue()
     now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    response = HttpResponse(mimetype="image/tif")
-    response['Content-Disposition'] = 'attachment; filename=%s_x%s_y%s_%s_template.tif'\
-        % (request.user.username, pos_x, pos_y, now)
     response.write(jpeg)
     return response
