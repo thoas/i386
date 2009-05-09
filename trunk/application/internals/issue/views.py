d68d136a33c886f31a43121a8e23e884862d1ce6
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-05-09 19:26:24 +0000

    add templatetags and book square

--- trunk/application/internals/issue/views.py
+++ trunk/application/internals/issue/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext_lazy as _
 
 from issue.models import Issue, ParticipateIssue
+from square.models import Square, SquareOpen
 
 @login_required
 def issues(request, template_name='issues.html'):
@@ -18,8 +19,16 @@ def issues(request, template_name='issues.html'):
 def issue(request, slug, template_name='issue_details.html'):
     """docstring for issues"""
     issue = get_object_or_404(Issue, slug=slug)
+    squares_open = SquareOpen.objects.all()
+    squares = Square.objects.all()
+    
+    t_squares_open = dict((square_open.coord, square_open) for square_open in squares_open)
+    t_squares = dict((square.coord, square) for square in squares)
+    
     return render_to_response(template_name, {
         'issue': issue,
+        't_squares_open': t_squares_open,
+        't_squares': t_squares,
         'nb_case_x': range(issue.nb_case_x),
         'nb_case_y': range(issue.nb_case_y)
     }, context_instance=RequestContext(request))
