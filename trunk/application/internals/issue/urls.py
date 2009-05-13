69da088e62e1abf73a4a46d9fdb19a55ba2cc4e3
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-05-13 12:52:12 +0000

    dry principe on views

--- trunk/application/internals/issue/urls.py
+++ trunk/application/internals/issue/urls.py
@@ -3,5 +3,5 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('issue.views',
     url(r'^$', 'issues', name='issue_list'),
-    url(r'^(?P<slug>[\w]+)/$', 'issue'),
+    url(r'^(?P<slug>[\w]+)/$', 'issue', name='issue'),
 )
--- trunk/application/internals/square/views.py
+++ trunk/application/internals/square/views.py
@@ -1,8 +1,3 @@
-import StringIO
-
-from os import unlink
-from os.path import join, exists
-from PIL import Image
 from datetime import datetime
 
 from django.shortcuts import render_to_response, get_object_or_404
@@ -15,38 +10,16 @@ from django.conf import settings
 
 from square.models import Square, SquareOpen
 from issue.models import Issue
-from square.constance import *
 from square.forms import SquareForm
 
-FORMAT_IMAGE = 'PNG'
-MIMETYPE_IMAGE = 'image/png'
+MIMETYPE_IMAGE = 'image/tiff'
 
 def book(request, issue, square_open):
-    image = Image.new('RGB', (1200, 1200), 'white')
-    neighbors_key = square_open.neighbors()
-    neighbors = Square.objects.neighbors(square_open)
-    
-    # creation d'un square
-    for neighbor in neighbors:
-        index = neighbors_key[neighbor.coord]
-        im = Image.open(neighbor.background_image_path.path)
-        
-        print '%s -> %s (%s)' % (CROP_POS[index], PASTE_POS[index], LITERAL[index])
-        crop = im.crop(CROP_POS[index])
-        image.paste(crop, PASTE_POS[index])
-
-    pos_x = square_open.pos_x
-    pos_y = square_open.pos_y
-    now = datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
-    template_name = '%s__x%s_y%s__%s__template.tif' % (request.user.username, pos_x, pos_y, now)
-    square = Square.objects.create(pos_x=pos_x, pos_y=pos_y,\
-                status=0, issue=issue, user=request.user, template_name=template_name)
-
+    square = Square.objects.create(pos_x=square_open.pos_x, pos_y=square_open.pos_x,\
+                status=0, issue=issue, user=request.user)
     SquareOpen.objects.neighbors_standby(square_open, True);
-
-    image.save(square.template_path(), format=FORMAT_IMAGE, quality=90)
-    image.filename = template_name
-    return template(request, image)
+    
+    return template(request, square.get_template())
 
 def release(request, issue, square_open):
     square = get_object_or_404(Square, pos_x=square_open.pos_x,\
@@ -60,11 +33,13 @@ def fill(request, issue, square_open):
                 pos_y=square_open.pos_y, issue=issue)
     if request.method == 'POST':
         datas = request.POST.copy()
-        form = SquareForm(request.POST, request.FILES, square)
+        #datas.appendlist('status', True)
+        form = SquareForm(datas, request.FILES, instance=square)
         if form.is_valid():
-            pass
+            form.save()
+            #return HttpResponseRedirect(reverse('issue', kwargs={'slug': issue.slug }))
     else:
-        form = SquareForm(square)
+        form = SquareForm(instance=square)
     return render_to_response('fill.html', {
         'square': square,
         'issue': issue,
@@ -87,13 +62,11 @@ def templates(request, template_name='templates.html'):
 @login_required
 def template(request, template):
     if isinstance(template, unicode):
-        template_path = join(settings.TEMPLATE_ROOT, template)
-        if not exists(template_path):
+        template = Square.retrieve_template(template)
+        if not template:
             raise Http404
-        template = Image.open(template_path)
     
-    buffer = StringIO.StringIO()
-    template.save(buffer, format=FORMAT_IMAGE, quality=90)
+    buffer = Square.buffer(template)
     response = HttpResponse(mimetype=MIMETYPE_IMAGE)
     response['Content-Disposition'] = 'attachment; filename=%s' % template.filename
     response.write(buffer.getvalue())
