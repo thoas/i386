edc9931915a82eb329c2ff718b2eada2e5a21fc9
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-05-19 16:53:16 +0000

    add gateway and middlewares

--- trunk/application/internals/issue/urls.py
+++ trunk/application/internals/issue/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+from django.conf import settings
 
-urlpatterns = patterns('issue.gateway',
-    url(r'^gateway/$', 'issueGateway')
-)
 
-urlpatterns += patterns('issue.views',
-    url(r'issues.(?P<format>xml|json|html)$', 'issues', {'template_name': 'issues.html'}, name='issue_list'),
-    url(r'^$', 'issues', {'format': 'html', 'template_name': 'issues.html'}, name='issue_list'),
-    url(r'^(?P<slug>[\w]+)/$', 'issue', name='issue'),
+urlpatterns = patterns('issue.views',
+    url(r'^issues.(?P<format>xml|json|html)$', 'issues', {'template_name': 'issues.html'}, name='issue_list'),
+    url(r'^$', 'issues', {'format': settings.DEFAULT_FORMAT, 'template_name': 'issues.html'}, name='issue_list'),
+    
+    url(r'^(?P<slug>[\w]+).(?P<format>xml|json|html)$', 'issue', {'template_name': 'issue_details.html'}, name='issue'),
+    url(r'^(?P<slug>[\w]+)$', 'issue', {'format': settings.DEFAULT_FORMAT, 'template_name': 'issue_details.html'}, name='issue'),
+    url(r'^gateway/$', 'issueGateway'),
 )
