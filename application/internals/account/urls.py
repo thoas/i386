2fbd7a1655bd086de7513e5f7967a980de6c53e3
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-05-20 00:07:35 +0000

    add amf protocol

--- application/internals/account/urls.py
+++ application/internals/account/urls.py
@@ -1,19 +1,32 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
-    url(r'^email/$', 'account.views.email', name='acct_email'),
-    url(r'^signup/(?P<confirmation_key>[a-zA-Z0-9]{10,20})/$', 'account.views.signup', name='acct_signup_key'),
-    url(r'^signup/$', 'account.views.signup', name='acct_signup'),
-    url(r'^login/$', 'account.views.login', name='acct_login'),
-    url(r'^password_change/$', 'account.views.password_change', name='acct_passwd'),
-    url(r'^invitations/$', 'account.views.invitations', name='acct_invitations'),
-    url(r'^invitations/(?P<confirmation_key>[a-zA-Z0-9]{10,20})/$', 'account.views.invitations', name='acct_invitations_key'),
-    url(r'^password_reset/$', 'account.views.password_reset', name='acct_passwd_reset'),
-    url(r'^timezone/$', 'account.views.timezone_change', name='acct_timezone_change'),
-    url(r'^language/$', 'account.views.language_change', name='acct_language_change'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}, name='acct_logout'),
+urlpatterns = patterns('account.views',
+    url(r'^email/$', 'email', {'template_name': 'email.html'}, name='acct_email'),
     
-    url(r'^confirm_email/(\w+)/$', 'emailconfirmation.views.confirm_email', name='acct_confirm_email'),
+    url(r'^signup/(?P<confirmation_key>[a-zA-Z0-9]{10,20})/$', 'signup', {'template_name': 'signup.html'}, name='acct_signup_key'),
+    url(r'^signup/$', 'signup', {'confirmation_key': '', 'template_name': 'signup.html'}, name='acct_signup'),
+    
+    url(r'^login/$', 'login', {'template_name': 'login.html'}, name='acct_login'),
+    
+    url(r'^password_change/$', 'password_change', {'template_name': 'password_change.html'}, name='acct_passwd'),
+    
+    url(r'^invitations/$', 'invitations', {'template_name': 'invitations.html', 'confirmation_key': ''}, name='acct_invitations'),
+    url(r'^invitations/(?P<confirmation_key>[a-zA-Z0-9]{10,20})/$', 'invitations',\
+        {'template_name': 'invitations.html'}, name='acct_invitations_key'),
+    
+    url(r'^password_reset/$', 'password_reset', name='acct_passwd_reset'),
+    
+    url(r'^timezone/$', 'timezone_change', {'template_name': 'timezone_change.html'}, name='acct_timezone_change'),
+    
+    url(r'^language/$', 'language_change', {'template_name': 'language_change.html'}, name='acct_language_change'),
     
     #(r'^validate/$', 'ajax_validation.views.validate', {'form_class': SignupForm}, 'signup_form_validate'),
+    
+    url(r'^gateway/$', 'accountGateway'),
+)
+
+urlpatterns += patterns('',
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}, name='acct_logout'),
+
+    url(r'^confirm_email/(\w+)/$', 'emailconfirmation.views.confirm_email', name='acct_confirm_email'),
 )
--- cli/test/gateway.py
+++ cli/test/gateway.py
@@ -4,3 +4,8 @@ gw = RemotingService('http://127.0.0.1:8000/issue/gateway/')
 service = gw.getService('issue')
 
 print service.issue('5x5')
+
+#gw = RemotingService('http://127.0.0.1:8000/account/gateway/')
+#service = gw.getService('account')
+
+#print service.login('thoas', 'toto')
