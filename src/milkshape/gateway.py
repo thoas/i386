dcc5385235cd2d4dcbe5e0582785bf6481cd97f2
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-06-21 20:01:59 +0000

    add centralized gateway

---
+++ src/milkshape/gateway.py
@@ -0,0 +1,24 @@
+import pyamf
+
+from pyamf.remoting.gateway.django import DjangoGateway
+
+from account.views import _login, _logout, _is_authenticated
+from square.views import _book, _release, _template, _fill
+from about.views import _contact
+from issue.views import _issues, _issue
+
+gateway = DjangoGateway({
+    'account.login': _login,
+    'account.logout': _logout,
+    'account.is_authenticated': _is_authenticated,
+    
+    'square.book': _book,
+    'square.release': _release,
+    'square.template': _template,
+    'square.fill': _fill,
+    
+    'issue.issues': _issues,
+    'issue.issue': _issue,
+    
+    'about.contact': _contact,
+})
