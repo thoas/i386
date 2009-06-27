bad2fb969b7fc179062a39a96029509fbb69b232
florent.messa <florent.messa@7f552156-ea58-11dd-9059-71fac19e68c6>
2009-06-27 11:50:54 +0000

    bug fix

--- trunk/src/milkshape/application/internals/issue/management/commands/fill.py
+++ trunk/src/milkshape/application/internals/issue/management/commands/fill.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import random
-import sys
-import shutil
 import Image
-import os
-import re
 import unittest
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'local_settings'
 
 from os.path import abspath, dirname, join
 from optparse import make_option
+from math import ceil
 
 from django.test.client import Client
 from django.core.urlresolvers import reverse
@@ -33,10 +27,13 @@ BASE_ROOT = dirname(abspath(__file__))
 class Command(BaseCommand):
     def handle(self, *args, **options):
         image_root = args[0]
-        c = Client()
-        result = c.login(username=ACCT_USERNAME, password=ACCT_PASSWORD)
+        self.client = Client()
+        result = self.client.login(username=ACCT_USERNAME, password=ACCT_PASSWORD)
         if result:
-            image = Image.open(image_root)
+            self.image = Image.open(image_root)
+            self.width, self.height = self.image.size
+            
+            
             """"
             print 'Issue\'s title?'
             title = sys.stdin.readline()
@@ -57,7 +54,7 @@ class Command(BaseCommand):
             margin = sys.stdin.readline()
             
             print 'How much squares to fill?'
-            until_fill = sys.stdin.readline()
+            self.until_fill = sys.stdin.readline()
             
             
             issue = Issue.objects.get_or_create(
@@ -77,64 +74,83 @@ class Command(BaseCommand):
                 issue=issue
             )
             
-            process(c, image, issue)
+            cpt = 0
+            process(c, cpt)
             """
             
-            fill(c, 0, 0, Issue.objects.get(slug="5x5"), image)
+            self.issue = Issue.objects.get(slug="5x5")
+            self.template_size = self.issue.size + (self.issue.margin * 2)
+            
+            self.max_square_x = ceil(self.width / self.issue.size)
+            self.max_square_y = ceil(self.height / self.issue.size)
+            
+    def process(self, cpt):  
+        squares_open = self.__squares_open()  
+        for square_open in squares_open:
+            book(square_open.pos_x, square_open.pos_y)
+            fill(square_open.pos_x, square_open.pos_y)
+            cpt += 1
+        if self.__squares_open().count() > 0 and cpt < self.until_fill:
+            process()
 
-def process(c, image, issue):    
-    for square_open in squares_open:
-        book(c, square_open.pos_x, square_open.pos_y, issue)
-        fill(c, square_open.pos_x, square_open.pos_y, issue, image)
+    def book(self, pos_x, pos_y):
+        response = c.get(reverse('square_book', kwargs={
+            'pos_x': pos_x,
+            'pos_y':pos_y,
+            'issue_slug': self.issue.slug
+        }))
     
-    if SquareOpen.objects.filter(is_standby=False, issue=self.issue).count() > 0:
-        process(c, image, issue)
+    def __squares_open():
+        return squares_open = SquareOpen.objects.filter(
+            is_standby=False, 
+            issue=self.issue, 
+            pos_x__lte=self.max_square_x,
+            pos_y__lte=self.max_square_y
+        )
+    
+    def fill(self, pos_x, pos_y):
+        left_x = pos_y * self.issue.size
+        paste_left_x = 0
+        paste_right_x = self.template_size
+    
+        top_y = pos_x * self.issue.size
+        paste_top_y = 0
+        paste_bottom_y = self.template_size
+    
+        if left_x == 0:
+            paste_left_x += self.issue.margin
+            right_x = left_x + self.issue.size + self.issue.margin
+        else:
+            left_x -= self.issue.margin
+            right_x = left_x + self.template_size
+    
+        if top_y == 0:
+            paste_top_y += self.issue.margin
+            bottom_y = top_y + self.issue.size + self.issue.margin
+        else:
+            top_y -= self.issue.margin
+            bottom_y = top_y + self.template_size
+    
+        if right_x >= self.width:
+            right_x -= self.issue.margin
+            paste_right_x -= self.issue.margin
+    
+        if bottom_y >= self.height:
+            bottom_y -= self.issue.margin
+            paste_bottom_y -= self.issue.margin
+    
+        crop = self.image.crop((left_x, top_y, right_x, bottom_y))
+        print (left_x, top_y, right_x, bottom_y)
+        paste_image = Image.new('RGB', (self.template_size, self.template_size), 'white')
+        print (paste_left_x, paste_top_y, paste_right_x, paste_bottom_y)
+        paste_image.paste(crop, (paste_left_x, paste_top_y, paste_right_x, paste_bottom_y))
+    
+        paste_image_path = join(settings.TMP_ROOT, '%d_%d.tif' % (pos_x, pos_y))
+        paste_image.save(paste_image_path, format='TIFF', quality=90)
 
-def book(c, pos_x, pos_y, issue):
-    response = c.get(reverse('square_book', kwargs={
-        'pos_x': pos_x,
-        'pos_y':pos_y,
-        'issue_slug': issue.slug
-    }))
-
-def fill(c, pos_x, pos_y, issue, image):
-    template_size = issue.size + issue.margin * 2
-    width, height = image.size
     
-    left_x = pos_y * issue.size
-    paste_left_x = 0
-    paste_right_x = template_size
-    
-    top_y = pos_x * issue.size
-    paste_top_y = 0
-    paste_bottom_y = template_size
-    
-    if left_x == 0:
-        paste_left_x += issue.margin
-        right_x = left_x + issue.size + issue.margin
-    else:
-        left_x -= issue.margin
-        right_x = left_x + template_size
-    
-    if top_y == 0:
-        paste_top_y += issue.margin
-        bottom_y = top_y + issue.size + issue.margin
-    else:
-        top_y -= issue.margin
-        bottom_y = top_y + template_size
-    
-    crop = image.crop((left_x, top_y, right_x, bottom_y))
-    print (left_x, top_y, right_x, bottom_y)
-    paste_image = Image.new('RGB', (template_size, template_size), 'white')
-    print (paste_left_x, paste_top_y, paste_right_x, paste_bottom_y)
-    paste_image.paste(crop, (paste_left_x, paste_top_y, paste_right_x, paste_bottom_y))
-    
-    paste_image_path = join(settings.TMP_ROOT, '%d_%d.jpg' % (pos_x, pos_y))
-    paste_image.save(paste_image_path, format='JPEG', quality=90)
-
-    
-    #response = c.post(reverse('square_fill', kwargs={
-    #    'pos_x': pos_x,
-    #    'pos_y': pos_y,
-    #    'issue_slug': issue.slug
-    #}), {'background_image': f})
+        #response = c.post(reverse('square_fill', kwargs={
+        #    'pos_x': pos_x,
+        #    'pos_y': pos_y,
+        #    'issue_slug': issue.slug
+        #}), {'background_image': f})
